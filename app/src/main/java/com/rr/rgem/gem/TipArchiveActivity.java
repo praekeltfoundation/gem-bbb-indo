fc624c51876944b7ee5f850268e8583a811dabfc
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-05 16:06:12 +0200

    Added TabHost layout to Tips Archive

--- app/src/main/java/com/rr/rgem/gem/TipArchiveActivity.java
+++ app/src/main/java/com/rr/rgem/gem/TipArchiveActivity.java
@@ -1,6 +1,9 @@
 package com.rr.rgem.gem;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TabHost;
 
 import com.rr.rgem.gem.models.Tip;
 import com.rr.rgem.gem.navigation.GEMNavigation;
@@ -13,7 +16,12 @@ import java.util.List;
  */
 public class TipArchiveActivity extends ApplicationActivity {
 
+    static String TAB_FAVOURITES = "Favourites";
+    static String TAB_ALL = "All";
+
     private GEMNavigation navigation;
+    private LinearLayout tipScreen;
+    private TabHost tabHost;
     private List<Tip> tips;
 
     @Override
@@ -21,8 +29,24 @@ public class TipArchiveActivity extends ApplicationActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        navigation = new GEMNavigation(this);
         Utils.toast(this, "starting Tips Archive activity");
+        navigation = new GEMNavigation(this);
+        tipScreen = (LinearLayout) navigation.addLayout(R.layout.tip_archive);
+
+        // Initialize Tabs
+        tabHost = (TabHost) tipScreen.findViewById(R.id.tipTabHost);
+        tabHost.setup();
+        TabHost.TabSpec spec;
+
+        spec = tabHost.newTabSpec(TAB_FAVOURITES);
+        spec.setContent(R.id.favouritesTab);
+        spec.setIndicator(TAB_FAVOURITES);
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec(TAB_ALL);
+        spec.setContent(R.id.allTab);
+        spec.setIndicator(TAB_ALL);
+        tabHost.addTab(spec);
     }
 
 }
