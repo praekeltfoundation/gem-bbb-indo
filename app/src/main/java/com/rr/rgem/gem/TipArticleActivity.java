856808f50ee03e4af28b34322f0dbd13f227c224
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-13 13:00:37 +0200

    Added progress indicator to Tips Article web view.

--- app/src/main/java/com/rr/rgem/gem/TipArticleActivity.java
+++ app/src/main/java/com/rr/rgem/gem/TipArticleActivity.java
@@ -1,8 +1,12 @@
 package com.rr.rgem.gem;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.rr.rgem.gem.navigation.GEMNavigation;
 import com.rr.rgem.gem.views.Utils;
@@ -21,7 +25,23 @@ public class TipArticleActivity extends ApplicationActivity {
         Utils.toast(this, "starting Tip Article activity");
         navigation = new GEMNavigation(this);
         articleScreen = (ViewGroup) navigation.addLayout(R.layout.tip_article);
+
+        final View tipProgress = findViewById(R.id.tipProgress);
+        //tipProgress.setVisibility(View.GONE);
         webView = (WebView) findViewById(R.id.tipWebView);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                tipProgress.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                tipProgress.setVisibility(View.GONE);
+            }
+        });
 
         Bundle extras = getIntent().getExtras();
         String url = extras.getString("url");
