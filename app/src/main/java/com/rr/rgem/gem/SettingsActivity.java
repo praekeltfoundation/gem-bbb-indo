f352448a4c31d967aed5e73f3fc3e71a421be256
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-26 13:11:46 +0200

    Clearing activity stack.

--- app/src/main/java/com/rr/rgem/gem/SettingsActivity.java
+++ app/src/main/java/com/rr/rgem/gem/SettingsActivity.java
@@ -40,7 +40,7 @@ public class SettingsActivity extends AppCompatActivity {
                     // Clear activity history stack
                     ((GEM) getApplication()).onUrlChanged();
                     Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent);
                     SettingsActivity.this.finish();
                 } else {
