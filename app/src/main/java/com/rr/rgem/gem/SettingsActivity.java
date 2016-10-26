104a62260cd214a221eca29e02ec2ad124c4fa9c
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-26 12:01:47 +0200

    Persisting url from SettingsActivity.

--- app/src/main/java/com/rr/rgem/gem/SettingsActivity.java
+++ app/src/main/java/com/rr/rgem/gem/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.rr.rgem.gem;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Wimpie Victor on 2016/10/26.
@@ -13,10 +17,35 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.settings);
 
         persisted = new Persisted(getApplicationContext());
 
+        EditText editViewUrl = (EditText) findViewById(R.id.editTextUrl);
+        editViewUrl.setText(persisted.loadUrl());
 
+        Button buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editViewUrl = (EditText) findViewById(R.id.editTextUrl);
+
+                String prevUrl = persisted.loadUrl();
+                String newUrl = editViewUrl.getText().toString();
+
+                persisted.saveUrl(newUrl);
+
+                // Settings Changed
+                if (!prevUrl.equals(newUrl)) {
+                    // Clear activity history stack
+                    Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    SettingsActivity.this.finish();
+                } else {
+                    // Return to previous activity
+                    SettingsActivity.this.finish();
+                }
+            }
+        });
     }
 }
