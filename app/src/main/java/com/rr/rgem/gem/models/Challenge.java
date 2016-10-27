a367181b1c6cabd1a85007c77cf197add283d0c7
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-10-27 17:05:04 +0200

    Update challenge model.

--- app/src/main/java/com/rr/rgem/gem/models/Challenge.java
+++ app/src/main/java/com/rr/rgem/gem/models/Challenge.java
@@ -1,14 +1,20 @@
 package com.rr.rgem.gem.models;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by chris on 9/14/2016.
  */
 public class Challenge {
+    private long id;
     private String name;
-    private String completed;
+    private Date activationDate;
+    private Date deactivationDate;
+    private String type;
+    private boolean completed = false;
+
     private List<Question> questions = new ArrayList<Question>();
 
     public String getName() {
@@ -27,11 +33,11 @@ public class Challenge {
         this.questions = questions;
     }
 
-    public String getCompleted() {
+    public boolean getCompleted() {
         return completed;
     }
 
-    public void setCompleted(String completed) {
+    public void setCompleted(boolean completed) {
         this.completed = completed;
     }
 }
