447d478faa426dc3188466da76842dad2b5a8f18
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-27 12:09:09 +0200

    Added sign out button to settings.

--- app/src/main/java/com/rr/rgem/gem/SettingsActivity.java
+++ app/src/main/java/com/rr/rgem/gem/SettingsActivity.java
@@ -7,7 +7,10 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.rr.rgem.gem.image.ImageStorage;
 import com.rr.rgem.gem.views.Utils;
 
 /**
@@ -26,6 +29,15 @@ public class SettingsActivity extends AppCompatActivity {
 
         EditText editViewUrl = (EditText) findViewById(R.id.editTextUrl);
         editViewUrl.setText(persisted.loadUrl(getString(R.string.default_service_url)));
+
+        Button buttonSignOut = (Button) findViewById(R.id.buttonSignOut);
+        buttonSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Utils.toast(SettingsActivity.this, "Logging out");
+                signOut();
+            }
+        });
 
         Button buttonSave = (Button) findViewById(R.id.buttonSave);
         buttonSave.setOnClickListener(new View.OnClickListener() {
@@ -47,15 +59,31 @@ public class SettingsActivity extends AppCompatActivity {
                 if (!prevUrl.equals(newUrl)) {
                     // Clear activity history stack
                     ((GEM) getApplication()).onUrlChanged();
-                    Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                    SettingsActivity.this.finish();
+                    clearHistoryAndStart();
                 } else {
                     // Return to previous activity
                     SettingsActivity.this.finish();
                 }
             }
         });
+    }
+
+    void signOut() {
+        persisted.clearUser();
+        persisted.clearToken();
+        persisted.setLoggedIn(false);
+        persisted.setRegistered(false);
+
+        ImageStorage storage = new ImageStorage(getApplicationContext(), "imageDir");
+        storage.clearDirectory();
+
+        clearHistoryAndStart();
+    }
+
+    void clearHistoryAndStart() {
+        Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        SettingsActivity.this.finish();
     }
 }
