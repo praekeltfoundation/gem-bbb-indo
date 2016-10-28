26f0f5cb8775b14fc0ba2c70fd1c0192d291896a
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-10-28 11:29:33 +0200

    Add participant answer model.

---
+++ app/src/main/java/com/rr/rgem/gem/models/ParticipantAnswer.java
@@ -0,0 +1,43 @@
+package com.rr.rgem.gem.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
+/**
+ * Created by Rudolph Jacobs on 2016-10-28.
+ */
+
+public class ParticipantAnswer {
+    long question;
+
+    @SerializedName("selected_option")
+    long selectedOption;
+
+    @SerializedName("date_answered")
+    Date dateAnswered = new Date();
+
+    public long getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(long question) {
+        this.question = question;
+    }
+
+    public long getSelectedOption() {
+        return selectedOption;
+    }
+
+    public void setSelectedOption(long selectedOption) {
+        this.selectedOption = selectedOption;
+    }
+
+    public Date getDateAnswered() {
+        return dateAnswered;
+    }
+
+    public void setDateAnswered(Date dateAnswered) {
+        this.dateAnswered = dateAnswered;
+    }
+}
