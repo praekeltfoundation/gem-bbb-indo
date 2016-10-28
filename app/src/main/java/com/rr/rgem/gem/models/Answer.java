85aa158ba0f3b30ac8677011fe3e0dc442fe2933
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-10-28 09:49:28 +0200

    Add 'correct' field to answer model.

--- app/src/main/java/com/rr/rgem/gem/models/Answer.java
+++ app/src/main/java/com/rr/rgem/gem/models/Answer.java
@@ -10,16 +10,19 @@ public class Answer {
     private long id;
     private String name;
     private String text;
+    private boolean correct = false;
 
     public long getId() {
         return id;
     }
-    public String getName() {
-        return name;
-    }
+
+    public String getName() { return name; }
+
     public String getText() {
         return text;
     }
+
+    public boolean getCorrect() { return correct; }
 
     public void setName(String name) {
         this.name = name;
