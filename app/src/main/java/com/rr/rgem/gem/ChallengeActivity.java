16841267cda1b9c0f1204a9fee36e2a05af06378
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-10-28 08:25:43 +0200

    Load example challenge in challenge activity.

--- app/src/main/java/com/rr/rgem/gem/ChallengeActivity.java
+++ app/src/main/java/com/rr/rgem/gem/ChallengeActivity.java
@@ -1,5 +1,6 @@
 package com.rr.rgem.gem;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,20 +13,28 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.google.gson.Gson;
 import com.rr.rgem.gem.answers.ChallengesAnswers;
 import com.rr.rgem.gem.controllers.JSONConversation;
 import com.rr.rgem.gem.controllers.common.Factory;
 import com.rr.rgem.gem.controllers.common.JSONController;
+import com.rr.rgem.gem.models.Challenge;
+import com.rr.rgem.gem.models.ConversationNode;
 import com.rr.rgem.gem.models.ConvoCallback;
 import com.rr.rgem.gem.navigation.GEMNavigation;
 import com.rr.rgem.gem.views.LeftRightConversation;
 import com.rr.rgem.gem.views.Message;
 import com.rr.rgem.gem.views.Utils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.Map;
 
 /**
@@ -44,48 +53,36 @@ public class ChallengeActivity extends ApplicationActivity{
     private ChallengesAnswers coachController;
     private ImageView currentImage;
     private boolean done;
+    private Challenge challenge;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         navigation = new GEMNavigation(this);
         Utils.toast(this, "starting current challenge activity");
-        coachScreen = (LinearLayout) navigation.addLayout(R.layout.conversational_layout);
-        contentLayout = (RelativeLayout) coachScreen.findViewById(R.id.container);
-        coachView = new LeftRightConversation(contentLayout);
-        //coachController = new JSONConversation(this, R.raw.challenges);
-        this.coachController = Factory.createChallenges(this,coachView);
-        coachController.setDoneCallback(new ConvoCallback() {
-            @Override
-            public String callback(Map<String, String> vars, Map<String, String> responses) {
-                Message summary = new Message(1, "2016", true, Message.ResponseType.FreeForm, null);
-                summary.setTitle("SUMMARY:");
-                coachView.addFreeFormPlain(summary);
+        Gson gson = new Gson();
+        challenge = gson.fromJson(loadJsonFromResources(this), Challenge.class);
+    }
 
-                for (String key: responses.keySet()) {
-                    Message msg = new Message(0, "2016", true, Message.ResponseType.FreeForm, null);
-                    msg.setTitle(key + ": " + responses.get(key));
-                    coachView.addFreeFormPlain(msg);
-                }
-
-                Button button1 = new Button(contentLayout.getContext());
-                button1.setText(R.string.challenge_next);
-                coachView.addRightView(button1, "name");
-                button1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        Intent intent = new Intent(ChallengeActivity.this,MainActivity.class);
-                        startActivity(intent);
-
-                    }
-                });
-                return null;
-
+    String loadJsonFromResources(Context context){
+        InputStream is = context.getResources().openRawResource(R.raw.challenges);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
             }
+        }catch (Exception e){
+        } finally {
+            try {
+                is.close();
+            }catch(Exception e){
+            }
+        }
 
-        });
-        //coachController.sendChallenges(this, coachView, null);
+        return writer.toString();
     }
 
     public  void goToMain() {
