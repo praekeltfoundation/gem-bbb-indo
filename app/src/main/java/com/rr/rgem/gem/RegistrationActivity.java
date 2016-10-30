29a5f5e75c57aac2a6c88248a4fd002fa74de7d1
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-30 15:14:23 +0200

    Refactored RegistrationActivity to use Butter Knife.

--- app/src/main/java/com/rr/rgem/gem/RegistrationActivity.java
+++ app/src/main/java/com/rr/rgem/gem/RegistrationActivity.java
@@ -1,12 +1,10 @@
 package com.rr.rgem.gem;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import com.rr.rgem.gem.service.AuthService;
 import com.rr.rgem.gem.service.ErrorUtil;
@@ -15,13 +13,15 @@ import com.rr.rgem.gem.service.WebServiceFactory;
 import com.rr.rgem.gem.service.errors.AuthError;
 import com.rr.rgem.gem.service.errors.RegistrationError;
 import com.rr.rgem.gem.service.model.AuthLogin;
-import com.rr.rgem.gem.service.model.AuthToken;
 import com.rr.rgem.gem.service.model.AuthTokenResponse;
 import com.rr.rgem.gem.service.model.Profile;
 import com.rr.rgem.gem.service.model.RegistrationResponse;
 import com.rr.rgem.gem.service.model.User;
 import com.rr.rgem.gem.views.Utils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -33,15 +33,22 @@ public class RegistrationActivity extends ApplicationActivity {
 
     private static final String TAG = "RegistrationActivity";
 
+    @BindView(R.id.editTextName) EditText editName;
+    @BindView(R.id.editTextMobile) EditText editMobile;
+    @BindView(R.id.editTextPassword) EditText editPassword;
+    @BindView(R.id.buttonRegister) Button buttonRegister;
+
     AuthService authService;
     ErrorUtil errorUtil;
+    Persisted persisted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_registration_page);
+        ButterKnife.bind(this);
 
-        final Persisted persisted = new Persisted(getSharedPreferences(Persisted.APP_PREFS,0));
+        persisted = new Persisted(getSharedPreferences(Persisted.APP_PREFS,0));
         Log.d(TAG, "Current token " + persisted.loadToken());
 
         if (persisted.isRegistered()) {
@@ -54,75 +61,66 @@ public class RegistrationActivity extends ApplicationActivity {
         WebServiceFactory factory = ((WebServiceApplication) getApplication()).getWebServiceFactory();
         authService = factory.createAuthService();
         errorUtil = new ErrorUtil(factory.createRetrofit());
+    }
 
-        final Button button = (Button) findViewById(R.id.buttonRegister);
-        button.setOnClickListener(new View.OnClickListener() {
+    private boolean isValidMobile(CharSequence phone){
+        return android.util.Patterns.PHONE.matcher(phone).matches();
+    }
 
-            private boolean isValidMobile(CharSequence phone){
-                return android.util.Patterns.PHONE.matcher(phone).matches();
+    private void toast(CharSequence message) {
+        Utils.toast(this, message);
+    }
+
+    @OnClick(R.id.buttonRegister)
+    void onRegistrationClick(Button button) {
+        if (editName.getText().length() < 4){
+            toast(getString(R.string.invalid_username_length));
+            return;
+        }
+
+        if (!isValidMobile(editMobile.getText())){
+            toast(getString(R.string.invalid_mobile_number));
+            return;
+        }
+
+        if (editPassword.getText().length() < 8){
+            toast(getString(R.string.invalid_password_length));
+            return;
+        }
+
+        //TODO: check for log in activity
+
+        final User user = new User();
+        user.setUsername(editName.getText().toString());
+        user.setPassword(editPassword.getText().toString());
+
+        Profile profile = new Profile();
+        profile.setMobile(editMobile.getText().toString());
+
+        user.setProfile(profile);
+        Log.d(RegistrationActivity.TAG, "Registering user: " + user);
+        Log.d(RegistrationActivity.TAG, "Password: " + user.getPassword());
+
+        RegistrationActivity.this.authService.register(user).enqueue(new Callback<RegistrationResponse>() {
+            @Override
+            public void onResponse(Call<RegistrationResponse> call, Response<RegistrationResponse> response) {
+                if (response.isSuccessful()) {
+                    Log.d(RegistrationActivity.TAG, "Registration successful");
+                    Utils.toast(getApplicationContext(), getString(R.string.congratulations_registered));
+                    persisted.setRegistered(true);
+                    // TODO: Should password be cleared before save?
+                    persisted.saveUser(user);
+                    retrieveToken(user);
+                } else {
+                    RegistrationError error = RegistrationActivity.this.errorUtil
+                            .parseError(response, RegistrationError.class);
+                    Log.d(RegistrationActivity.TAG, "Registration failed " + error.toString());
+                }
             }
 
-            private void toast(CharSequence text){
-                Context context = getApplicationContext();
-                Utils.toast(context,text);
-
-            }
-
-            public void onClick(View v) {
-                TextView editName = (TextView)findViewById(R.id.editTextName);
-                TextView editMobile = (TextView)findViewById(R.id.editTextMobile);
-                TextView editPassword = (TextView)findViewById(R.id.editTextPassword);
-
-                if(editName.getText().length() < 4){
-                    toast(getString(R.string.invalid_username_length));
-                    return;
-                }
-
-                if(!isValidMobile(editMobile.getText())){
-                    toast(getString(R.string.invalid_mobile_number));
-                    return;
-                }
-
-                if(editPassword.getText().length() < 8){
-                    toast(getString(R.string.invalid_password_length));
-                    return;
-                }
-
-                //TODO: check for log in activity
-
-                final User user = new User();
-                user.setUsername(editName.getText().toString());
-                user.setPassword(editPassword.getText().toString());
-
-                Profile profile = new Profile();
-                profile.setMobile(editMobile.getText().toString());
-
-                user.setProfile(profile);
-                Log.d(RegistrationActivity.TAG, "Registering user: " + user);
-                Log.d(RegistrationActivity.TAG, "Password: " + user.getPassword());
-
-                RegistrationActivity.this.authService.register(user).enqueue(new Callback<RegistrationResponse>() {
-                    @Override
-                    public void onResponse(Call<RegistrationResponse> call, Response<RegistrationResponse> response) {
-                        if (response.isSuccessful()) {
-                            Log.d(RegistrationActivity.TAG, "Registration successful");
-                            Utils.toast(getApplicationContext(), getString(R.string.congratulations_registered));
-                            persisted.setRegistered(true);
-                            // TODO: Should password be cleared before save?
-                            persisted.saveUser(user);
-                            retrieveToken(user);
-                        } else {
-                            RegistrationError error = RegistrationActivity.this.errorUtil
-                                    .parseError(response, RegistrationError.class);
-                            Log.d(RegistrationActivity.TAG, "Registration failed " + error.toString());
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<RegistrationResponse> call, Throwable t) {
-                        Log.d(RegistrationActivity.TAG, "Registration error", t);
-                    }
-                });
+            @Override
+            public void onFailure(Call<RegistrationResponse> call, Throwable t) {
+                Log.d(RegistrationActivity.TAG, "Registration error", t);
             }
         });
     }
@@ -134,8 +132,6 @@ public class RegistrationActivity extends ApplicationActivity {
     }
 
     void retrieveToken(User user) {
-        // TODO: Log user in immediately after successful registration
-        final Persisted persisted = new Persisted(this);
         final AuthLogin login = AuthLogin.fromUser(user);
 
         authService.createToken(login).enqueue(new Callback<AuthTokenResponse>() {
