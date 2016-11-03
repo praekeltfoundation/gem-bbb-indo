34b8dc03030b74a669bd18d96b6881e41ab858a4
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-11-03 16:13:20 +0200

    Parse text on buttons as well.

--- app/src/main/java/com/rr/rgem/gem/controllers/JSONConversation.java
+++ app/src/main/java/com/rr/rgem/gem/controllers/JSONConversation.java
@@ -238,7 +238,7 @@ public class JSONConversation {
         } else if (current.type == ConversationNode.NodeType.choice) {
             Map<String,View.OnClickListener> listeners = new HashMap<String,View.OnClickListener>();
             for (final ConversationNode.AnswerNode choice: current.answers){
-                listeners.put(choice.value, new View.OnClickListener() {
+                listeners.put(parseStringVars(choice.text), new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         state = State.Correct;
