c79ed4e9302900c1fdcc9b7eab58771d83b6f01c
Herman Barnardt <hbarnardt@retrorabbit.co.za>
2016-11-05 14:28:00 +0200

    Added Challenge API

--- app/src/main/java/com/nike/dooit/Constants.java
+++ app/src/main/java/com/nike/dooit/Constants.java
@@ -6,6 +6,7 @@ package com.nike.dooit;
 
 public class Constants {
 
-    public static final String BASE_URL = "http://gem-bbb.seed.p16n.org";
+//    public static final String BASE_URL = "http://gem-bbb.seed.p16n.org";
+    public static final String BASE_URL = "http://ec2-52-19-36-136.eu-west-1.compute.amazonaws.com";
 
 }
