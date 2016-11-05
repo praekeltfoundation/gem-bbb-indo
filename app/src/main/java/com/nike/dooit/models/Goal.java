3e809ca511372b7bb57941f55cee5c272beae3a3
Herman Barnardt <hbarnardt@retrorabbit.co.za>
2016-11-05 14:41:47 +0200

    Added prelim Goal model

---
+++ app/src/main/java/com/nike/dooit/models/Goal.java
@@ -0,0 +1,56 @@
+package com.nike.dooit.models;
+
+import org.joda.time.DateTime;
+
+/**
+ * Created by herman on 2016/11/05.
+ */
+
+public class Goal {
+
+    private String name;
+    private double value;
+    private String image;
+    private DateTime start_date;
+    private DateTime end_date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public DateTime getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(DateTime start_date) {
+        this.start_date = start_date;
+    }
+
+    public DateTime getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(DateTime end_date) {
+        this.end_date = end_date;
+    }
+}
