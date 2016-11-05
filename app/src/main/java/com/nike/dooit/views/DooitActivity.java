4cb474ef66e994f7e9e7a69fed088066b8449b8c
wscheffer <wscheffer@retrorabbit.co.za>
2016-11-05 13:06:11 +0200

    Added Basic Welkom Screen interaction

---
+++ app/src/main/java/com/nike/dooit/views/DooitActivity.java
@@ -0,0 +1,13 @@
+package com.nike.dooit.views;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wsche on 2016/11/05.
+ */
+
+public class DooitActivity extends AppCompatActivity {
+    public void dismissDialog() {
+
+    }
+}
--- app/src/main/java/com/nike/dooit/views/onboarding/RegistrationActivity.java
+++ app/src/main/java/com/nike/dooit/views/onboarding/RegistrationActivity.java
@@ -1,9 +1,12 @@
 package com.nike.dooit.views.onboarding;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.nike.dooit.R;
+import com.nike.dooit.views.helpers.activity.DooitActivityBuilder;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -11,5 +14,22 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public static class Builder extends DooitActivityBuilder<Builder> {
+        protected Builder(Context context) {
+            super(context);
+        }
+
+        public static Builder create(Context context) {
+            Builder builder = new Builder(context);
+            return builder;
+        }
+
+        @Override
+        protected Intent createIntent(Context context) {
+            return new Intent(context, RegistrationActivity.class);
+        }
+
     }
 }
