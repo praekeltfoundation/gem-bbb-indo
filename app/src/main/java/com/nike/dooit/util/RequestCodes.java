cd9ab8a72647c35dbe2a2b75db05a826a4af5210
wscheffer <wscheffer@retrorabbit.co.za>
2016-11-06 15:06:24 +0200

    Added Onboarding Profile Image Screen

---
+++ app/src/main/java/com/nike/dooit/util/RequestCodes.java
@@ -0,0 +1,8 @@
+package com.nike.dooit.util;
+
+/**
+ * Created by wsche on 2016/11/06.
+ */
+public class RequestCodes {
+    public static final int REPONSE_CAMERA_REQUEST_PROFILE_IMAGE = 1000;
+}
