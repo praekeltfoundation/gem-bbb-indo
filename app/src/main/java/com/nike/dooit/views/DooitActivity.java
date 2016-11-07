1e841abd97260fb3cdcd9b629e733358e236f884
Bernhard Muller <bmuller@retrorabbit.co.za>
2016-11-07 10:43:00 +0200

    Updated profile image displaying and persisting

--- app/src/main/java/com/nike/dooit/views/DooitActivity.java
+++ app/src/main/java/com/nike/dooit/views/DooitActivity.java
@@ -1,7 +1,10 @@
 package com.nike.dooit.views;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -36,6 +39,22 @@ public class DooitActivity extends AppCompatActivity {
 
     public void dismissDialog() {
 
+    }
+
+    public Uri getRealPathFromURI(Uri contentUri) {
+        try {
+            Cursor cursor = getContentResolver().query(contentUri, null, null, null, null);
+            if (cursor == null) {
+                return contentUri;
+            } else {
+                cursor.moveToFirst();
+                int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                return Uri.parse(cursor.getString(index));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return contentUri;
     }
 
     public Locale getLocal() {
