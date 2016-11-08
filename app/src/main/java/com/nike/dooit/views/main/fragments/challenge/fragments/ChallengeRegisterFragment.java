065ccab04cdb92353c37b81212a8cba64095c16b
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-11-08 14:39:10 +0200

    Alter arguments to challenge registration fragment.

--- app/src/main/java/com/nike/dooit/views/main/fragments/challenge/fragments/ChallengeRegisterFragment.java
+++ app/src/main/java/com/nike/dooit/views/main/fragments/challenge/fragments/ChallengeRegisterFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.nike.dooit.R;
+import com.nike.dooit.models.Challenge;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,12 +22,10 @@ import com.nike.dooit.R;
 public class ChallengeRegisterFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_CHALLENGE = "challenge";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Challenge challenge;
 
     private OnFragmentInteractionListener mListener;
 
@@ -38,16 +37,12 @@ public class ChallengeRegisterFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ChallengeRegisterFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ChallengeRegisterFragment newInstance(String param1, String param2) {
+    public static ChallengeRegisterFragment newInstance() {
         ChallengeRegisterFragment fragment = new ChallengeRegisterFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +51,7 @@ public class ChallengeRegisterFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            challenge = getArguments().getParcelable(ARG_CHALLENGE);
         }
     }
 
