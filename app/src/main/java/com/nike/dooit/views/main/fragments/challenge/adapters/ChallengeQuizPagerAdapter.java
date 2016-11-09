73c794b7e86e1f84fa05d7c93e8fdb8e65f501e1
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-11-09 08:01:48 +0200

    Remove unnecessary challenge enumerator.

--- app/src/main/java/com/nike/dooit/views/main/fragments/challenge/adapters/ChallengeQuizPagerAdapter.java
+++ app/src/main/java/com/nike/dooit/views/main/fragments/challenge/adapters/ChallengeQuizPagerAdapter.java
@@ -1,25 +1,13 @@
 package com.nike.dooit.views.main.fragments.challenge.adapters;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.nike.dooit.R;
 import com.nike.dooit.models.Challenge;
 import com.nike.dooit.models.Question;
-import com.nike.dooit.views.main.fragments.challenge.ChallengeViewPagerPositions;
-import com.nike.dooit.views.main.fragments.ChallengeFragment;
-import com.nike.dooit.views.main.fragments.TargetFragment;
-import com.nike.dooit.views.main.fragments.TipsFragment;
-import com.nike.dooit.views.main.fragments.bot.BotFragment;
 import com.nike.dooit.views.main.fragments.challenge.fragments.ChallengeQuizQuestionFragment;
-import com.nike.dooit.views.main.fragments.challenge.fragments.ChallengeRegisterFragment;
 
 import java.util.List;
 
