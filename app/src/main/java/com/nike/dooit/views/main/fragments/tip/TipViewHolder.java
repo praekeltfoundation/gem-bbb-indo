aa34976d53e7a025dbf06b75ebcb9d0caa0f02ef
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-11-10 17:32:50 +0200

    Added icons to Tip Cards.

--- app/src/main/java/com/nike/dooit/views/main/fragments/tip/TipViewHolder.java
+++ app/src/main/java/com/nike/dooit/views/main/fragments/tip/TipViewHolder.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.text.SpannableString;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +27,12 @@ public class TipViewHolder extends RecyclerView.ViewHolder {
     @BindView(R.id.card_tip_title)
     TextView titleView;
 
+    @BindView(R.id.card_tip_fav)
+    ImageView favView;
+
+    @BindView(R.id.card_tip_share)
+    ImageView shareView;
+
     public TipViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
@@ -34,6 +41,16 @@ public class TipViewHolder extends RecyclerView.ViewHolder {
     @OnClick(R.id.card_tip_title)
     public void startArticle(View view) {
         Toast.makeText(view.getContext(), titleView.getText().toString() + " clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.card_tip_fav)
+    public void favouriteTip(View view) {
+        Toast.makeText(view.getContext(), titleView.getText().toString() + " faved", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.card_tip_share)
+    public void shareTip(View view) {
+        Toast.makeText(view.getContext(), titleView.getText().toString() + " shared", Toast.LENGTH_SHORT).show();
     }
 
     public void setImageUri(Uri uri) {
