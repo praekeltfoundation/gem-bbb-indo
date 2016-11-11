1a31d0371f6c8a044950173cdcc041f2b72618c4
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-11-11 12:07:50 +0200

    Give challenge type more useful mappings.

--- app/src/main/java/com/nike/dooit/models/enums/ChallengeType.java
+++ app/src/main/java/com/nike/dooit/models/enums/ChallengeType.java
@@ -2,12 +2,40 @@ package com.nike.dooit.models.enums;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Rudolph Jacobs on 2016-11-11.
  */
 
 public enum ChallengeType {
-    @SerializedName("quiz") QUIZ,
-    @SerializedName("picture") PICTURE,
-    @SerializedName("freeform") FREEFORM
+    INVALID(0, null),
+    @SerializedName("quiz") QUIZ(1, "quiz"),
+    @SerializedName("picture") PICTURE(2, "picture"),
+    @SerializedName("freeform") FREEFORM(3, "freeform");
+
+    private static Map<Integer, ChallengeType> map = new HashMap<Integer, ChallengeType>();
+
+    static {
+        for (ChallengeType cardType : ChallengeType.values()) {
+            map.put(cardType.value, cardType);
+        }
+    }
+
+    private final int value;
+    private final String name;
+
+    ChallengeType(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public static ChallengeType getValueOf(int value) {
+        return map.containsKey(value) ? map.get(value) : INVALID;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
