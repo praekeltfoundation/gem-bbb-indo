793470a0c024147826f16c8379d57dba43eca79f
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-11-12 17:32:37 +0200

    Add challenge loading indicator.

--- app/src/main/java/com/nike/dooit/views/main/fragments/ChallengeFragment.java
+++ app/src/main/java/com/nike/dooit/views/main/fragments/ChallengeFragment.java
@@ -59,7 +59,7 @@ public class ChallengeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_challenge, container, false);
+        View view = inflater.inflate(R.layout.fragment_challenge_loading, container, false);
         ButterKnife.bind(this, view);
 
 ////        if (persisted.hasCurrentChallenge()) {
