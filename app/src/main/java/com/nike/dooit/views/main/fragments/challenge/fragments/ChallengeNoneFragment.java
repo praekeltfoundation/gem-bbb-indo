c6eee74f4fb5e3de883b88d2415bbb51c361c8e2
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-11-14 15:24:41 +0200

    Add support for displaying a message on no-challenge view.

--- app/src/main/java/com/nike/dooit/views/main/fragments/challenge/fragments/ChallengeNoneFragment.java
+++ app/src/main/java/com/nike/dooit/views/main/fragments/challenge/fragments/ChallengeNoneFragment.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.nike.dooit.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +20,14 @@ import com.nike.dooit.R;
  * create an instance of this fragment.
  */
 public class ChallengeNoneFragment extends Fragment {
+    public static final String ARG_MESSAGE = "message";
+
+    private String message = null;
+    private Unbinder unbinder = null;
+
+    @BindView(R.id.fragment_challenge_none_text)
+    TextView messageText;
+
     public ChallengeNoneFragment() {
         // Required empty public constructor
     }
@@ -24,7 +37,6 @@ public class ChallengeNoneFragment extends Fragment {
      * this fragment using the provided parameters.
      * @return A new instance of fragment ChallengeNoneFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static ChallengeNoneFragment newInstance() {
         ChallengeNoneFragment fragment = new ChallengeNoneFragment();
         Bundle args = new Bundle();
@@ -32,16 +44,38 @@ public class ChallengeNoneFragment extends Fragment {
         return fragment;
     }
 
+    public static ChallengeNoneFragment newInstance(String message) {
+        ChallengeNoneFragment fragment = new ChallengeNoneFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_MESSAGE, message);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (this.getArguments() != null) {
+            this.message = getArguments().getString(ARG_MESSAGE);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_challenge_none, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        if (message != null) {
+            messageText.setText(message);
+        }
         return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
+        super.onDestroyView();
+    }
 }
