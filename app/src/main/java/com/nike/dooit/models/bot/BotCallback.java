909652fcb6ac9e4b4ab40258cab003e63bbc4426
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-11-20 11:30:51 +0200

    Added bot callback.

---
+++ app/src/main/java/com/nike/dooit/models/bot/BotCallback.java
@@ -0,0 +1,9 @@
+package com.nike.dooit.models.bot;
+
+/**
+ * Created by Wimpie Victor on 2016/11/20.
+ */
+
+public interface BotCallback {
+    void onDone();
+}
---
+++ app/src/main/java/com/nike/dooit/views/main/fragments/target/callbacks/GoalAddCallback.java
@@ -0,0 +1,18 @@
+package com.nike.dooit.views.main.fragments.target.callbacks;
+
+import com.nike.dooit.models.bot.BotCallback;
+
+/**
+ * Created by Wimpie Victor on 2016/11/20.
+ */
+
+public class GoalAddCallback implements BotCallback {
+
+    public GoalAddCallback() {
+    }
+
+    @Override
+    public void onDone() {
+
+    }
+}
