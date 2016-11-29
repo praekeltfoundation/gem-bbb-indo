a1a520d6d87307514fedf0e200f33a55eee4c735
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-11-29 21:08:52 +0200

    Add challenge fragment state enum.

---
+++ app/src/main/java/org/gem/indo/dooit/views/main/fragments/challenge/ChallengeFragmentState.java
@@ -0,0 +1,13 @@
+package org.gem.indo.dooit.views.main.fragments.challenge;
+
+/**
+ * Created by Rudolph Jacobs on 2016-11-29.
+ */
+
+public enum ChallengeFragmentState {
+    LOADING,
+    QUIZ,
+    FREEFORM,
+    PICTURE,
+    NONE
+}
