b711a8ee61a33ff3a1183e7235b9443e81599d1e
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-11-30 13:06:48 +0200

    Make quiz state parcelable.

--- app/src/main/java/org/gem/indo/dooit/models/challenge/QuizChallengeQuestionState.java
+++ app/src/main/java/org/gem/indo/dooit/models/challenge/QuizChallengeQuestionState.java
@@ -1,17 +1,65 @@
 package org.gem.indo.dooit.models.challenge;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 
 /**
  * Created by Rudolph Jacobs on 2016-11-29.
  */
 
-public class QuizChallengeQuestionState implements Serializable {
-    public long optionId = -1;
+public class QuizChallengeQuestionState implements Parcelable, Serializable {
+
+    /*************
+     * Variables *
+     *************/
+
     public boolean completed = false;
+
+    public long optionId = -1;
+
+
+    /****************
+     * Constructors *
+     ****************/
 
     public QuizChallengeQuestionState(long optionId, boolean completed) {
         this.optionId = optionId;
         this.completed = completed;
     }
+
+
+    /************************
+     * Parcelable interface *
+     ************************/
+
+    protected QuizChallengeQuestionState(Parcel in) {
+        completed = in.readByte() != 0x00;
+        optionId = in.readLong();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeByte((byte) (completed ? 0x01 : 0x00));
+        dest.writeLong(optionId);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<QuizChallengeQuestionState> CREATOR = new Parcelable.Creator<QuizChallengeQuestionState>() {
+        @Override
+        public QuizChallengeQuestionState createFromParcel(Parcel in) {
+            return new QuizChallengeQuestionState(in);
+        }
+
+        @Override
+        public QuizChallengeQuestionState[] newArray(int size) {
+            return new QuizChallengeQuestionState[size];
+        }
+    };
 }
