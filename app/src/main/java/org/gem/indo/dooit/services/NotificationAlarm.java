589e2f32fbc0c609f67921eb53ee916cc2220466
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-12-06 14:46:16 +0200

    Interval constants.

--- app/src/main/java/org/gem/indo/dooit/services/NotificationAlarm.java
+++ app/src/main/java/org/gem/indo/dooit/services/NotificationAlarm.java
@@ -13,6 +13,9 @@ import android.support.v4.content.WakefulBroadcastReceiver;
 public class NotificationAlarm extends WakefulBroadcastReceiver {
 
     private static final String TAG = NotificationAlarm.class.getName();
+    // milliseconds * seconds * minutes * hours * days
+    public static final long WEEK = 1000 * 60 * 60 * 24 * 7;
+    public static final long TEN_SECONDS = 1000 * 10; // For dev
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -23,18 +26,17 @@ public class NotificationAlarm extends WakefulBroadcastReceiver {
     public static void setAlarm(Context context) {
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, NotificationAlarm.class);
-        PendingIntent pending = PendingIntent.getBroadcast(context, 0, intent, 0);
+        PendingIntent pending = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         // RTC because we don't want to wake the device up.
         // Interval is (milliseconds * seconds * minutes)
-        int interval = 1000 * 10;
-        manager.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), interval, pending);
+        manager.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), TEN_SECONDS, pending);
     }
 
     public static void cancelAlarm(Context context) {
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, NotificationAlarm.class);
-        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
+        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         manager.cancel(sender);
     }
 }
