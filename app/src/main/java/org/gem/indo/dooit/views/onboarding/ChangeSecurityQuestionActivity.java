7844c26149f5953579605f874bf32f981e5c1e62
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-12-07 18:00:04 +0200

    Add security question change screen.

---
+++ app/src/main/java/org/gem/indo/dooit/views/onboarding/ChangeSecurityQuestionActivity.java
@@ -0,0 +1,16 @@
+package org.gem.indo.dooit.views.onboarding;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.gem.indo.dooit.R;
+
+public class ChangeSecurityQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_change_security_question);
+    }
+
+}
