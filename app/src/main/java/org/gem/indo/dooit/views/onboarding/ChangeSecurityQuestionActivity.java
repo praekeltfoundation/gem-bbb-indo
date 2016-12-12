22fb83a37aeffea4c138942c43a8f00fc1aff2e2
Rudolph Jacobs <rjacobs@retrorabbit.co.za>
2016-12-12 17:28:18 +0200

    Provide error feedback on security question change page.

--- app/src/main/java/org/gem/indo/dooit/views/onboarding/ChangeSecurityQuestionActivity.java
+++ app/src/main/java/org/gem/indo/dooit/views/onboarding/ChangeSecurityQuestionActivity.java
@@ -3,6 +3,7 @@ package org.gem.indo.dooit.views.onboarding;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -18,6 +19,7 @@ import org.gem.indo.dooit.api.managers.UserManager;
 import org.gem.indo.dooit.api.responses.EmptyResponse;
 import org.gem.indo.dooit.helpers.SquiggleBackgroundHelper;
 import org.gem.indo.dooit.views.DooitActivity;
+import org.gem.indo.dooit.views.custom.WigglyEditText;
 import org.gem.indo.dooit.views.helpers.activity.DooitActivityBuilder;
 
 import javax.inject.Inject;
@@ -56,10 +58,10 @@ public class ChangeSecurityQuestionActivity extends DooitActivity {
     View background;
 
     @BindView(R.id.activity_onboarding_change_secq_question_textbox)
-    EditText questionBox;
+    WigglyEditText questionBox;
 
     @BindView(R.id.activity_onboarding_change_secq_answer_textbox)
-    EditText answerBox;
+    WigglyEditText answerBox;
 
     @BindView(R.id.activity_onboarding_change_secq_button)
     Button submitButton;
@@ -118,16 +120,25 @@ public class ChangeSecurityQuestionActivity extends DooitActivity {
 
     @OnClick(R.id.activity_onboarding_change_secq_button)
     protected void submit() {
-        String question = questionBox.getText().toString();
-        String answer = answerBox.getText().toString();
+        boolean valid = true;
+        String question = questionBox.getEditText();
+        String answer = answerBox.getEditText();
 
-        if (TextUtils.isEmpty(question) || TextUtils.isEmpty(answer)) {
-            Toast.makeText(this, "Must enter a question and answer.", Toast.LENGTH_SHORT).show();
-            return;
+        if (TextUtils.isEmpty(question)) {
+            valid = false;
+            questionBox.setMessageText(getString(R.string.label_required));
+        } else if (question.length() < minFieldLength) {
+            valid = false;
+            questionBox.setMessageText(String.format(getString(org.gem.indo.dooit.R.string.msg_security_question_too_short), minFieldLength));
         }
 
-        if (question.length() < minFieldLength) {
-            Toast.makeText(this, String.format("Question must be at least %1$d letters long.", minFieldLength), Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(answer)) {
+            valid = false;
+            answerBox.setMessageText(getString(R.string.label_required));
+        }
+
+        if (!valid) {
+            Snackbar.make(submitButton, R.string.msg_fix_indicated_fields, Snackbar.LENGTH_SHORT).show();
             return;
         }
 
@@ -137,7 +148,7 @@ public class ChangeSecurityQuestionActivity extends DooitActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(ChangeSecurityQuestionActivity.this, "Could not send new question.", Toast.LENGTH_SHORT).show();
+                        Snackbar.make(submitButton, R.string.security_question_could_not_send, Snackbar.LENGTH_SHORT).show();
                     }
                 });
             }
@@ -147,7 +158,7 @@ public class ChangeSecurityQuestionActivity extends DooitActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(ChangeSecurityQuestionActivity.this, "Security question changed.", Toast.LENGTH_SHORT).show();
+                        Snackbar.make(submitButton, R.string.security_question_change_successful, Snackbar.LENGTH_SHORT).show();
                         ChangeSecurityQuestionActivity.this.finish();
                     }
                 });
