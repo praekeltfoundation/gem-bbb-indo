12d0615e6fcf86170a63f882fa7e8f61158969ee
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-12-30 08:32:29 +0200

    Removed unused code

--- app/src/main/java/org/gem/indo/dooit/validatior/Validator.java
+++ app/src/main/java/org/gem/indo/dooit/validatior/Validator.java
@@ -1,6 +1,5 @@
 package org.gem.indo.dooit.validatior;
 
-//import android.support.v7.app.AppCompatDelegate;
 
 /**
  * Created by frede on 2016/12/28.
@@ -10,11 +9,7 @@ public abstract class Validator {
     protected int responseText;
     protected boolean valid;
 
-//    static {
-//        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-//    }
-
-    protected Validator(){
+    protected Validator() {
 
     }
 }
