205d41a9410300fb8e2485143c55aa51c4859b35
Wimpie Victor <wvictor@retrorabbit.co.za>
2017-01-10 13:43:18 +0200

    Added outstanding fields to controller.

--- app/src/main/java/org/gem/indo/dooit/controllers/survey/BaselineSurveyController.java
+++ app/src/main/java/org/gem/indo/dooit/controllers/survey/BaselineSurveyController.java
@@ -32,7 +32,36 @@ public class BaselineSurveyController extends SurveyController {
             "survey_baseline_a_school_name", // Inline text
             "survey_baseline_a_city", // Inline text
             "survey_baseline_q05_job_month",
-            "survey_baseline_q06_job_earning_range"
+            "survey_baseline_q06_job_earning_range",
+            "survey_baseline_q07_job_status",
+            "survey_baseline_q08_shared_ownership",
+            "survey_baseline_q09_business_earning_range",
+            "survey_baseline_q10_save",
+            "survey_baseline_q11_savings_frequency",
+            "survey_baseline_q12_savings_where",
+            "survey_baseline_q13_savings_3_months",
+            "survey_baseline_q14_saving_education",
+            "survey_baseline_q15_job_hunt",
+            "survey_baseline_q16_emergencies",
+            "survey_baseline_q17_invest",
+            "survey_baseline_q18_family",
+            "survey_baseline_q19_clothes_food",
+            "survey_baseline_q21_gadgets",
+            "survey_baseline_q22_friends",
+            "survey_baseline_q23_mobile_frequency",
+            "survey_baseline_q24_mobile_most_use",
+            "survey_baseline_q25_mobile_least_use",
+            "survey_baseline_q26_mobile_own",
+            "survey_baseline_q27_mobile_use_opinion",
+            "survey_baseline_q27_1_friends",
+            "survey_baseline_q27_2_family",
+            "survey_baseline_q27_3_community",
+            "survey_baseline_q28_mobile_credit",
+            "survey_baseline_q29_assets",
+            "survey_baseline_q29_1_desktop",
+            "survey_baseline_q29_2_laptop",
+            "survey_baseline_q29_3_mobile_no_data",
+            "survey_baseline_q29_4_mobile_data"
     };
 
     public BaselineSurveyController(Context context) {
