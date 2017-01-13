892ba203a3ca16844ae891e6b6ab99c4b272fc7b
Wimpie Victor <wvictor@retrorabbit.co.za>
2017-01-13 11:20:04 +0200

    Changed Terms & Conditions url

--- app/src/production/java/org/gem/indo/dooit/Constants.java
+++ app/src/production/java/org/gem/indo/dooit/Constants.java
@@ -9,8 +9,8 @@ public class Constants {
     public static final Boolean DEBUG = false;
     public static final String BASE_URL = "http://bbb-prd.gem.molo.unicore.io";
 
-    public static final String TERMS_URL = BASE_URL + "/terms-conditions/";
-    public static final String PRIVACY_URL = BASE_URL + "/privacy/";
+    public static final String TERMS_URL = BASE_URL + "/agreements/terms-conditions/";
+    public static final String PRIVACY_URL = BASE_URL + "/agreements/privacy/";
 
     public static final String FILE_PROVIDER = BuildConfig.APPLICATION_ID + ".fileprovider";
 }
