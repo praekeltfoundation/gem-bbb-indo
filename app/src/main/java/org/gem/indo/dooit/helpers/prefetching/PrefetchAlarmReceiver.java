3f725b43f36c5dafc77af6f9e5718d15c46f3a55
ReinhardtCromhout <RCromhout@retrorabbit.co.za>
2017-02-13 12:29:02 +0200

    added DI of Badge Api to Prefetch Alarm Receiver

--- app/src/main/java/org/gem/indo/dooit/helpers/prefetching/PrefetchAlarmReceiver.java
+++ app/src/main/java/org/gem/indo/dooit/helpers/prefetching/PrefetchAlarmReceiver.java
@@ -1,18 +1,29 @@
 package org.gem.indo.dooit.helpers.prefetching;
 
+import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
+import org.gem.indo.dooit.DooitApplication;
+import org.gem.indo.dooit.api.managers.BadgeManager;
+
+import javax.inject.Inject;
 
 /**
  * Created by Reinhardt on 2017/02/10.
  */
 
 public class PrefetchAlarmReceiver extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            prefetchBadgeImages(context);
-        }
+
+    @Inject
+    BadgeManager badgeManager;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ((DooitApplication) context.getApplicationContext()).component.inject(this);
+        prefetchBadgeImages(context);
+    }
 
     private void prefetchBadgeImages(Context context) {
         //TODO
