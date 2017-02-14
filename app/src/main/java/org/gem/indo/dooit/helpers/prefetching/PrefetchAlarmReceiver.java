09ffdb93b5fc48976f98ef8fef3f4c35a504b21b
ReinhardtCromhout <RCromhout@retrorabbit.co.za>
2017-02-14 09:54:57 +0200

    Async code to cache the badge images

--- app/src/main/java/org/gem/indo/dooit/helpers/prefetching/PrefetchAlarmReceiver.java
+++ app/src/main/java/org/gem/indo/dooit/helpers/prefetching/PrefetchAlarmReceiver.java
@@ -1,14 +1,21 @@
 package org.gem.indo.dooit.helpers.prefetching;
 
-import android.app.Application;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
 import org.gem.indo.dooit.DooitApplication;
+import org.gem.indo.dooit.api.DooitAPIError;
+import org.gem.indo.dooit.api.DooitErrorHandler;
 import org.gem.indo.dooit.api.managers.BadgeManager;
+import org.gem.indo.dooit.api.responses.AuthenticationResponse;
+import org.gem.indo.dooit.api.responses.BadgeImageUrlsResponse;
+import org.gem.indo.dooit.helpers.images.DraweeHelper;
 
 import javax.inject.Inject;
+
+import rx.functions.Action1;
 
 /**
  * Created by Reinhardt on 2017/02/10.
@@ -19,6 +26,8 @@ public class PrefetchAlarmReceiver extends BroadcastReceiver {
     @Inject
     BadgeManager badgeManager;
 
+    private String [] imageUrls;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         ((DooitApplication) context.getApplicationContext()).component.inject(this);
@@ -26,6 +35,26 @@ public class PrefetchAlarmReceiver extends BroadcastReceiver {
     }
 
     private void prefetchBadgeImages(Context context) {
-        //TODO
+        badgeManager.getBadgeUrls(new DooitErrorHandler() {
+            @Override
+            public void onError(DooitAPIError error) {
+
+            }
+        }).subscribe(new Action1<BadgeImageUrlsResponse>() {
+            @Override
+            public void call(BadgeImageUrlsResponse response) {
+                imageUrls = response.urls;
+                (new FetchImagesThread()).run();
+            }
+        });
+    }
+
+    private class FetchImagesThread implements Runnable {
+        @Override
+        public void run() {
+            for (String url : imageUrls) {
+                DraweeHelper.cacheImage(Uri.parse(url));
+            }
+        }
     }
 }
