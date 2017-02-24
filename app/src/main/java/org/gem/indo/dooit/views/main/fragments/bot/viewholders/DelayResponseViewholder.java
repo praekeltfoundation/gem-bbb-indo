0d9ca6862f8c88a67d122bd6c50643b2a84647e4
ReinhardtCromhout <RCromhout@retrorabbit.co.za>
2017-02-24 12:51:35 +0200

    Edited layout design

--- app/src/main/java/org/gem/indo/dooit/views/main/fragments/bot/viewholders/DelayResponseViewholder.java
+++ app/src/main/java/org/gem/indo/dooit/views/main/fragments/bot/viewholders/DelayResponseViewholder.java
@@ -1,20 +1,31 @@
 package org.gem.indo.dooit.views.main.fragments.bot.viewholders;
 
 import android.view.View;
+import com.github.channguyen.adv.AnimatedDotsView;
 
+import org.gem.indo.dooit.R;
 import org.gem.indo.dooit.models.bot.Node;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Reinhardt on 2017/02/24.
  */
 
 public class DelayResponseViewholder extends BaseBotViewHolder<Node> {
-    
+
+    @BindView(R.id.delay_dots)
+    AnimatedDotsView dots;
+
     @Override
     protected void populateModel() {
     }
 
     public DelayResponseViewholder(View itemView) {
         super(itemView);
+        ButterKnife.bind(this, itemView);
+        dots.startAnimation();
+
     }
 }
