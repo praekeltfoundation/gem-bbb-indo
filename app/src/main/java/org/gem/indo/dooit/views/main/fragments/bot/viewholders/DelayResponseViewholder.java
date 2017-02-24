af072ec89ae8ca0068572405257c8946c0407710
ReinhardtCromhout <RCromhout@retrorabbit.co.za>
2017-02-24 10:49:08 +0200

    Basic View for delay response

---
+++ app/src/main/java/org/gem/indo/dooit/views/main/fragments/bot/viewholders/DelayResponseViewholder.java
@@ -0,0 +1,20 @@
+package org.gem.indo.dooit.views.main.fragments.bot.viewholders;
+
+import android.view.View;
+
+import org.gem.indo.dooit.models.bot.Node;
+
+/**
+ * Created by Reinhardt on 2017/02/24.
+ */
+
+public class DelayResponseViewholder extends BaseBotViewHolder<Node> {
+    
+    @Override
+    protected void populateModel() {
+    }
+
+    public DelayResponseViewholder(View itemView) {
+        super(itemView);
+    }
+}
