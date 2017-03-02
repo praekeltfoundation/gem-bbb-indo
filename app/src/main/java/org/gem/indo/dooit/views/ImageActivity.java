d8036d9815f3f703427e2e6034dbb3714c2c08a4
ReinhardtCromhout <RCromhout@retrorabbit.co.za>
2017-03-02 13:19:47 +0200

    Move bitmap handling to backroung

--- app/src/main/java/org/gem/indo/dooit/views/ImageActivity.java
+++ app/src/main/java/org/gem/indo/dooit/views/ImageActivity.java
@@ -11,6 +11,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Environment;
 import android.os.Looper;
@@ -169,47 +170,10 @@ public abstract class ImageActivity extends DooitActivity {
     }
 
     private void handleImageResult(Intent data, int requestCodes) {
-        // When the camera is provided with an existing file beforehand, the intent is null. The
-        // pre-created image can be found via `imageUri`.
-        if (data != null) {
-            // No predefined image file was created
-            imageUri = data.getData();
-            if (imageUri == null) {
-                try {
-                    // This is the case where the camera only returns a thumbnail
-                    ContentResolver cR = this.getContentResolver();
-                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-
-                    boolean isUIThread = Looper.myLooper() == Looper.getMainLooper();
-
-                    //Note:  System.gc() is simply a hint to the garbage collector that you want it to do a collection.
-                    System.gc();
-
-                    bitmap = checkScreenOrientation(imagePath, bitmap, requestCodes);
-
-                    Log.d("IMAGE_TESTS", "Bitmap size : " + bitmap.getByteCount());
-                    imageUri = Uri.parse(MediaStore.Images.Media.insertImage(cR, bitmap, "", ""));
-                } catch (Throwable ex) {
-
-                }
-            }
-        }
-
-        if (TextUtils.isEmpty(imagePath)) {
-            // MediaUriHelper does not work when uri points to temp image file
-            try {
-                imagePath = MediaUriHelper.getPath(this, imageUri);
-                CrashlyticsHelper.log(this.getClass().getSimpleName(), "handleImageResult : ",
-                        "Context: " + this + " imageUri :" + imageUri);
-            } catch (NullPointerException nullException) {
-                CrashlyticsHelper.logException(nullException);
-            }
-        }
-
-        processImage(requestCodes);
-
-        ContentResolver cR = this.getContentResolver();
-        onImageResult(cR.getType(imageUri), imageUri, imagePath);
+        Object[] params = new Object[2];
+        params[0] = data;
+        params[1] = requestCodes;
+        new HandleImage().execute(params);
     }
 
     /**
@@ -218,61 +182,7 @@ public abstract class ImageActivity extends DooitActivity {
      */
 
     private void processImage(int requestCodes) {
-        FileOutputStream outStream = null;
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-
-        try {
-            File downscaledFile = createImageFile();
-            outStream = new FileOutputStream(downscaledFile);
-            Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
-
-            if (bitmap == null) {
-                Log.e(TAG, "Failed to load existing bitmap during downscale");
-                return;
-            }
-            /*
-            updateHeapValues();
-            if(availHeapSizeInMB < SCALE_FACTOR_FOR_BITMAP_SIZE * ((bitmap.getByteCount()/1024)/1024)){
-                //Note:  System.gc() is simply a hint to the garbage collector that you want it to do a collection.
-                System.gc();
-            }
-            */
-            boolean isUIThread = Looper.myLooper() == Looper.getMainLooper();
-
-
-            //Note:  System.gc() is simply a hint to the garbage collector that you want it to do a collection.
-            System.gc();
-
-            //Here the orientation is checked and corrected if needed
-            bitmap = checkScreenOrientation(imagePath, bitmap, requestCodes);
-
-            //Note:  System.gc() is simply a hint to the garbage collector that you want it to do a collection.
-            System.gc();
-
-            //Here the image is scaled to an acceptable size
-            bitmap = ImageScaler.scale(bitmap, maxImageWidth, maxImageHeight);
-            //file is written out
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outStream);
-
-            Log.d(TAG, String.format("Downscaled image dimensions: (%d, %d) %dKB",
-                    bitmap.getWidth(), bitmap.getHeight(), bitmap.getByteCount() / 1024));
-
-            // Set uri and filepath to new downscaled image
-            imagePath = downscaledFile.getAbsolutePath();
-            imageUri = FileProvider.getUriForFile(this, Constants.FILE_PROVIDER, downscaledFile);
-        } catch (IOException e) {
-            Toast.makeText(this, "Unable to do image rotation", Toast.LENGTH_LONG).show();
-            Log.e(TAG, "Unable to create temporary downscaled image file", e);
-            CrashlyticsHelper.log(this.getClass().getSimpleName(), " processImage : ", "an IOException");
-        } finally {
-            try {
-                if (outStream != null)
-                    outStream.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Failed to close outstream", e);
-            }
-        }
+        new ProcessImage().execute(requestCodes);
     }
 
     public static Bitmap rotateImage(Bitmap source, float angle) {
@@ -376,13 +286,127 @@ public abstract class ImageActivity extends DooitActivity {
      */
     protected abstract void onImageResult(String mediaType, Uri imageUri, String imagePath);
 
-    /**
-     * This function is used to check the available heap space for the app
-     */
-    private void updateHeapValues(){
-        usedMemInMB=(runtime.totalMemory() - runtime.freeMemory()) / 1048576L;
-        maxHeapSizeInMB=runtime.maxMemory() / 1048576L;
-        availHeapSizeInMB = maxHeapSizeInMB - usedMemInMB;
+
+    private class ProcessImage extends AsyncTask<Integer, Integer, Long> {
+        @Override
+        protected Long doInBackground(Integer...id) {
+            int requestCodes = id[0];
+
+            FileOutputStream outStream = null;
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+            try {
+                File downscaledFile = createImageFile();
+                outStream = new FileOutputStream(downscaledFile);
+                Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
+
+                if (bitmap == null) {
+                    Log.e(TAG, "Failed to load existing bitmap during downscale");
+                    return Long.valueOf(0);
+                }
+
+                boolean isUIThread = Looper.myLooper() == Looper.getMainLooper();
+
+
+                //Note:  System.gc() is simply a hint to the garbage collector that you want it to do a collection.
+                System.gc();
+
+                //Here the orientation is checked and corrected if needed
+                bitmap = checkScreenOrientation(imagePath, bitmap, requestCodes);
+
+                //Note:  System.gc() is simply a hint to the garbage collector that you want it to do a collection.
+                System.gc();
+
+                //Here the image is scaled to an acceptable size
+                bitmap = ImageScaler.scale(bitmap, maxImageWidth, maxImageHeight);
+                //file is written out
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outStream);
+
+                Log.d(TAG, String.format("Downscaled image dimensions: (%d, %d) %dKB",
+                        bitmap.getWidth(), bitmap.getHeight(), bitmap.getByteCount() / 1024));
+
+                // Set uri and filepath to new downscaled image
+                imagePath = downscaledFile.getAbsolutePath();
+                imageUri = FileProvider.getUriForFile(ImageActivity.this, Constants.FILE_PROVIDER, downscaledFile);
+            } catch (IOException e) {
+                Toast.makeText(ImageActivity.this, "Unable to do image rotation", Toast.LENGTH_LONG).show();
+                Log.e(TAG, "Unable to create temporary downscaled image file", e);
+                CrashlyticsHelper.log(this.getClass().getSimpleName(), " processImage : ", "an IOException");
+            } finally {
+                try {
+                    if (outStream != null)
+                        outStream.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "Failed to close outstream", e);
+                }
+            }
+            return Long.valueOf(0);
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+        protected void onPostExecute(Long result) {
+
+        }
+    }
+
+    private class HandleImage extends AsyncTask<Object, Integer, Long> {
+        @Override
+        protected Long doInBackground(Object...params) {
+            Intent data = (Intent)params[0];
+            int requestCodes = (Integer) params[1];
+            // When the camera is provided with an existing file beforehand, the intent is null. The
+            // pre-created image can be found via `imageUri`.
+            if (data != null) {
+                // No predefined image file was created
+                imageUri = data.getData();
+                if (imageUri == null) {
+                    try {
+                        // This is the case where the camera only returns a thumbnail
+                        ContentResolver cR = ImageActivity.this.getContentResolver();
+                        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+                        boolean isUIThread = Looper.myLooper() == Looper.getMainLooper();
+
+                        //Note:  System.gc() is simply a hint to the garbage collector that you want it to do a collection.
+                        System.gc();
+
+                        bitmap = checkScreenOrientation(imagePath, bitmap, requestCodes);
+
+                        Log.d("IMAGE_TESTS", "Bitmap size : " + bitmap.getByteCount());
+                        imageUri = Uri.parse(MediaStore.Images.Media.insertImage(cR, bitmap, "", ""));
+                    } catch (Throwable ex) {
+
+                    }
+                }
+            }
+
+            if (TextUtils.isEmpty(imagePath)) {
+                // MediaUriHelper does not work when uri points to temp image file
+                try {
+                    imagePath = MediaUriHelper.getPath(ImageActivity.this, imageUri);
+                    CrashlyticsHelper.log(this.getClass().getSimpleName(), "handleImageResult : ",
+                            "Context: " + this + " imageUri :" + imageUri);
+                } catch (NullPointerException nullException) {
+                    CrashlyticsHelper.logException(nullException);
+                }
+            }
+
+            processImage(requestCodes);
+
+            ContentResolver cR = ImageActivity.this.getContentResolver();
+            onImageResult(cR.getType(imageUri), imageUri, imagePath);
+            return Long.valueOf(0);
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+        protected void onPostExecute(Long result) {
+
+        }
     }
 
 }
