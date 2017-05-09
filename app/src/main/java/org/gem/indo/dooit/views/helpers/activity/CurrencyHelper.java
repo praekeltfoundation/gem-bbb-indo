7be5f49331867e3d8d9fe9aa801e1063b7f7a045
Chad Garrett <cgarrett@retrorabbit.co.za>
2017-05-09 11:00:32 +0200

    Added regex to format currency according to spec

--- app/src/main/java/org/gem/indo/dooit/views/helpers/activity/CurrencyHelper.java
+++ app/src/main/java/org/gem/indo/dooit/views/helpers/activity/CurrencyHelper.java
@@ -6,6 +6,7 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Currency;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 /**
  * Created by wsche on 2016/11/23.
@@ -15,16 +16,25 @@ public class CurrencyHelper {
     public static double DEFAULT_VALUE = 0.0f;
     private static Locale indonesia = new Locale("in", "ID");
 
+    private static Pattern currencyFormat = Pattern.compile("^Rp[.]?\\s*(.+)$");
+
     public static String format(Object o) {
         try {
-            // Lock currency symbols to Indonesian Rupiah
-            return NumberFormat.getCurrencyInstance(indonesia).format(Double.valueOf(String.valueOf(o))) + ",-";
-            //return NumberFormat.getCurrencyInstance().format(Double.valueOf((String.valueOf(o))));
+            // Requested currency format is 'Rp. XX.XXX,-'
+            // Number formatter for Indonesian locale returns 'RpXX.XXX'
+            // Regex formats currency as requested
+
+            String indonesianFormat = NumberFormat.getCurrencyInstance(indonesia).format(Double.valueOf(String.valueOf(o)));
+            indonesianFormat = indonesianFormat.replaceFirst("^Rp[.]?\\s*(.+)(,-)?$", "Rp. $1,-");
+            return indonesianFormat;
+
+//            return NumberFormat.getCurrencyInstance(indonesia).format(Double.valueOf(String.valueOf(o)));
+//            return NumberFormat.getCurrencyInstance().format(Double.valueOf((String.valueOf(o))));
         } catch (Exception e) {
             Crashlytics.log("Error converting number to currency [" + String.valueOf(o) + "]");
             Crashlytics.logException(e);
         }
-        return NumberFormat.getCurrencyInstance().format(DEFAULT_VALUE) + ",-";
+        return NumberFormat.getCurrencyInstance().format(DEFAULT_VALUE);
     }
 
     public static String getCurrencySymbol() {
